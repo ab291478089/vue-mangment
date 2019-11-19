677a5c8aa8dbd55f5b292c930343770e1f8c0775
lixiang <lixiang@email.com>
2019-11-19 12:53:10 +0800

    'second'

---
+++ server/client/src/views/Index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        初始化页面
+    </div>
+</template>
+<script>
+export default {
+  name: 'index',
+  components: {}
+
+}
+</script>
+<style lang="less" scoped>
+
+</style>
